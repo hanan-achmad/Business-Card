20cacb6133cee340e426b4954f4842d1197f1480
hanan-achmad <hananachmad8@gmail.com>
2023-10-18 23:08:19 +0800

    Projek Business Card

---
+++ app/src/main/java/com/example/hanansbusinesscard/ui/theme/Theme.kt
@@ -0,0 +1,37 @@
+package com.example.hanansbusinesscard.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColors
+import androidx.compose.material3.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+        primary = Purple200,
+        primaryVariant = Purple700,
+        secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+        primary = Purple500,
+        primaryVariant = Purple700,
+        secondary = Teal200
+)
+
+@Composable
+fun BusinessCardTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+    )
+}
